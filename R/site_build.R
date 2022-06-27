6ace0b2fdcca9a9557d63f2ae259eea4f2241962
Anthony Davidson <anthony.davidson@canberra.edu.au>
2022-06-27 22:04:01 +1000

    adding test

--- R/site_build.R
+++ R/site_build.R
@@ -1,4 +1,13 @@
 #site build
 # July2022
 # output_dir <- "./output"
-# rmarkdown::render("index.Rmd", output_dir = "./docs")
+rmarkdown::render("index.Rmd", output_dir = "./docs")
+
+??create_report
+create_report(iris, config = configure_report(add_plot_str = FALSE))
+
+devtools::install_github("boxuancui/DataExplorer", ref = "develop")
+
+#pandoc issues
+Sys.setenv(RSTUDIO_PANDOC = "/usr/bin/pandoc")
+if (file.exists('~/.Rprofile')) source('~/.Rprofile')
